8140e636ed2348db00149e7f399dea5d2498fc6c
Istvan Szukacs <istvan@streambrightdata.com>
2016-08-28 12:54:13 +0200

    Maybe

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
   }
   :dependencies [
     [org.clojure/clojure "1.8.0"]
+    [org.clojure/math.combinatorics "0.1.3"]
     [quil "2.4.0"]
   ]
   :main dejong.core)
--- src/dejong/core.clj
+++ src/dejong/core.clj
@@ -1,49 +1,26 @@
 (ns dejong.core
-  (:require [quil.core :as q]
-            [quil.middleware :as m])
+  (:require 
+    [quil.core :as q]
+    [quil.middleware :as m]
+    [clojure.math.combinatorics :as combo])
     (:gen-class))
 
-
-
 (defn sierpinski-triangle 
-  [[x y]]
-  (let 
-    [ x2 (/ x 2)
-      y2 (/ y 2) ]
-    
-    (rand-nth
-      [ [     x2          y2        ]
-        [ (* -1  x2)      y2        ]
-        [ (+  x2 1)       y2        ] ] )))
+  [[xa1 ya1 xa2 ya2] [xb1 yb1 xb2 yb2]] 
+  [(/ (+ xa1 xa2) 2) (/ (+ ya1 ya2) 2) (/ (+ xb1 xb2) 2) (/ (+ yb1 yb2)  2)])
 
-
-(defn line
-  []
-  (for [x (take 100 (iterate inc 0))]
-    [x x]))
-
-(def tria1 (for [x (range -400 1)] [x (+ (* 2 x) 400)]))
-
-(def tria2 (for [x (range 0 401)] [x (+ (* -2 x) 400)]))
-
-(def tria3 (for [x (range -400 401)] [x -398]))
-
-(def trian (lazy-cat tria1 tria2 tria3))
-
-(defn s-t [n] (take n (repeatedly #(sierpinski-triangle (rand-nth trian)))))
+(def tria1 '([-400 -400 0 400] [0 400 400 -400] [-400 -400 400 -400]))
+(def tria2 (map #(sierpinski-triangle (first %1) (second %1)) (combo/combinations tria1 2)))
+(def tria3 (sierpinski-triangle [-400 -400 0 200] [-200 -400 0 200]))
 
 (defn draw []
   (q/background 255)
   ; move origin point to centre of the sketch
   ; by default origin is in the left top corner
   (q/with-translation [(/ (q/width) 2) (/ (q/height) 2)]
-    
-    (let [coords (s-t 1000)]
-      (doseq [ [x1 y1 x2 y2] '([-400 -400 0 400] [0 400 400 -400] [-400 -400 400 -400]) ]
-        ;         x       y
-        (q/line   x1       (* -1 y1) x2 (* -1 y2) )))))
-
-
+    (doseq [ [x1 y1 x2 y2] (lazy-cat tria1 tria2 tria3)]
+    ;         x    y
+    (q/line   x1  (* -1 y1) x2 (* -1 y2) ))))
 
 ; run sketch
 (defn show-window 
@@ -51,7 +28,6 @@
   (q/defsketch trigonometry
     :size [800 800]
     :draw draw))
-
 
 (defn -main 
   [& args]
