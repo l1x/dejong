cc5820dc9067854c4af10ee30b73d9146edaf455
Istvan Szukacs <istvan@streambrightdata.com>
2016-08-28 11:19:50 +0200

    Adding trying different frawing methods

--- src/dejong/core.clj
+++ src/dejong/core.clj
@@ -3,43 +3,56 @@
             [quil.middleware :as m])
     (:gen-class))
 
-(defn setup []
-  (q/frame-rate 60)
-  (q/color-mode :hsb)
-  {:color 0
-   :angle 0})
 
-(defn update-state [state]
-  {:color (mod (+ (:color state) 0.7) 255)
-   :angle (+ (:angle state) 0.01)})
 
-(defn draw-state [state]
-  (println state)
-  ; Clear the sketch by filling it with light-grey color.
-  (q/background 240)
-  ; Set circle color.
-  (q/fill (:color state) 255 255)
-  ; Calculate x and y coordinates of the circle.
-  (let [angle (:angle state)
-        x (* 150 (q/cos angle))
-        y (* 150 (q/sin angle))]
-    ; Move origin point to the center of the sketch.
-    (q/with-translation [(/ (q/width) 2)
-                         (/ (q/height) 2)]
-      ; Draw the circle.
-      (q/ellipse x y 200 200))))
+(defn sierpinski-triangle 
+  [[x y]]
+  (let 
+    [ x2 (/ x 2)
+      y2 (/ y 2) ]
+    
+    (rand-nth
+      [ [     x2          y2        ]
+        [ (* -1  x2)      y2        ]
+        [ (+  x2 1)       y2        ] ] )))
 
-(defn -main [& args]
-  (q/defsketch hello-quil
-    :title "You spin my circle right round"
-    :size [1000 800]
-    ; setup function called only once, during sketch initialization.
-    :setup setup
-    ; update-state is called on each iteration before draw-state.
-    :update update-state
-    :draw draw-state
-    :features [:keep-on-top]
-    ; This sketch uses functional-mode middleware.
-    ; Check quil wiki for more info about middlewares and particularly
-    ; fun-mode.
-    :middleware [m/fun-mode]))
+
+(defn line
+  []
+  (for [x (take 100 (iterate inc 0))]
+    [x x]))
+
+(def tria1 (for [x (range -400 1)] [x (+ (* 2 x) 400)]))
+
+(def tria2 (for [x (range 0 401)] [x (+ (* -2 x) 400)]))
+
+(def tria3 (for [x (range -400 401)] [x -398]))
+
+(def trian (lazy-cat tria1 tria2 tria3))
+
+(defn s-t [n] (take n (repeatedly #(sierpinski-triangle (rand-nth trian)))))
+
+(defn draw []
+  (q/background 255)
+  ; move origin point to centre of the sketch
+  ; by default origin is in the left top corner
+  (q/with-translation [(/ (q/width) 2) (/ (q/height) 2)]
+    
+    (let [coords (s-t 1000)]
+      (doseq [ [x1 y1 x2 y2] '([-400 -400 0 400] [0 400 400 -400] [-400 -400 400 -400]) ]
+        ;         x       y
+        (q/line   x1       (* -1 y1) x2 (* -1 y2) )))))
+
+
+
+; run sketch
+(defn show-window 
+  []
+  (q/defsketch trigonometry
+    :size [800 800]
+    :draw draw))
+
+
+(defn -main 
+  [& args]
+  (show-window))
